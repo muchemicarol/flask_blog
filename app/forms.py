6052da28d53289833d97e762026dde8f5746e3e2
Wambui <cmcayo4@gmail.com>
2018-09-19 22:43:30 +0300

    User Account and Profile

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed  # Changing the profile picture.
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import  DataRequired, Length, Email, EqualTo, ValidationError
 from app.models import User
@@ -32,3 +34,30 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')  # Allows user to stay logged in after the browser closes using a secure cookie.
     submit = SubmitField('Log In')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture',
+                        validators=[FileAllowed(['jpeg', 'png'])])
+    submit = SubmitField('Update')
+
+    # Brings about the uniqueness factor of the username.
+    # Once a user inputs an already existing username, an error message is raised.
+    def validate_username(self, username):
+        # Ensures that changes are allowed if the given username and email are different.
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That Username is taken, Please choose a different one')
+
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken, Please choose a different one')
