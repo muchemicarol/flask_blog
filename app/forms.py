af0e0445629b39fb9e4cc0ae609f32aac29ce9c3
Wambui <cmcayo4@gmail.com>
2018-09-19 14:32:27 +0300

    User Authentication

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import  DataRequired, Length, Email, EqualTo
-
+from wtforms.validators import  DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -13,6 +13,18 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    # Brings about the uniqueness factor of the username.
+    # Once a user inputs an already existing username, an error message is raised.
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That Username is taken, Please choose a different one')
+
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken, Please choose a different one')
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
